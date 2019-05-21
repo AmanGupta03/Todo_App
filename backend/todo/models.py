aab47fac08b17478517002902a39e92af01dc60f
AmanGupta03 <theamangupta27@gmail.com>
2019-05-21 23:08:16 +0530

    updated models.py

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -2,12 +2,10 @@ from django.db import models
 
 # Todo model
 class Todo(models.Model):
-    title = models.CharField(max_length = 150)
+    title = models.CharField(max_length=150)
     description = models.TextField()
-    completed = models.BooleanField(default = False)
+    completed = models.BooleanField(default=False)
     objects = models.Manager()
 
     def _str_(self):
         return self.title
-
-
