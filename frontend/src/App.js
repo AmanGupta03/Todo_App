b87318172085bf2628ad4fbc24afca7b90911bde
AmanGupta03 <theamangupta27@gmail.com>
2019-05-22 11:05:50 +0530

    App.js updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,118 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import React, { Component } from "react";
+const todoItems = [
+  {
+      id: 1,
+      title: "competitive programming",
+      description: "solve 5 or 6 problem from codeforces",
+      completed: false
+  },
+  {
+    id: 2,
+    title: "React",
+    description: "Read few articles/tutorial related to React",
+    completed: false
+  },
+  {
+    id: 3,
+    title:  "todo project",
+    description: "work on this todo project",
+    completed: true
+  },
+  {
+    id: 4,
+    title: "articles",
+    description: "Read few articles from medium",
+    completed: false
+  },
+  {
+    id: 5,
+    title: "sports",
+    description: "visit friend's house to play.",
+    completed: false
+  },
+  {
+    id: 6,
+    title: "new todo",
+    description: "something random",
+    completed: true
+  }
+];
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewCompleted: false,
+      todoList: todoItems
+    };
+  }
+  displayCompleted = status => {
+    if (status) {
+      return this.setState({ viewCompleted: true });
+    }
+    return this.setState({ viewCompleted: false });
+  };
+  renderTabList = () => {
+    return (
+      <div className="my-5 tab-list">
+        <span
+          onClick={() => this.displayCompleted(true)}
+          className={this.state.viewCompleted ? "active" : ""}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          complete
+        </span>
+        <span
+          onClick={() => this.displayCompleted(false)}
+          className={this.state.viewCompleted ? "" : "active"}
+        >
+          Incomplete
+        </span>
+      </div>
+    );
+  };
+  renderItems = () => {
+    const { viewCompleted } = this.state;
+    const newItems = this.state.todoList.filter(
+      item => item.completed === viewCompleted
+    );
+    return newItems.map(item => (
+      <li
+        key={item.id}
+        className="list-group-item d-flex justify-content-between align-items-center"
+      >
+        <span
+          className={`todo-title mr-2 ${
+            this.state.viewCompleted ? "completed-todo" : ""
+          }`}
+          title={item.description}
+        >
+          {item.title}
+        </span>
+        <span>
+          <button className="btn btn-secondary mr-2"> Edit </button>
+          <button className="btn btn-danger">Delete </button>
+        </span>
+      </li>
+    ));
+  };
+  render() {
+    return (
+      <main className="content">
+        <h1 className="text-white text-uppercase text-center my-4">Todo app</h1>
+        <div className="row ">
+          <div className="col-md-6 col-sm-10 mx-auto p-0">
+            <div className="card p-3">
+              <div className="">
+                <button className="btn btn-primary">Add task</button>
+              </div>
+              {this.renderTabList()}
+              <ul className="list-group list-group-flush">
+                {this.renderItems()}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
 }
-
 export default App;
